177b18a0e9158d9d47c5e2763c7339155e139c40
Luis Pagliaro <lpagliaro@Luiss-MacBook-Pro.local>
2017-04-11 16:18:22 -0300

    Changed google analytics code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+declare let ga: Function;
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -8,4 +10,12 @@ import 'rxjs/add/operator/catch';
     templateUrl: './app.component.html'
 })
 export class AppComponent {
+    constructor(public router: Router) {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                ga('set', 'page', event.urlAfterRedirects);
+                ga('send', 'pageview');
+            }
+        });
+    }
 }
