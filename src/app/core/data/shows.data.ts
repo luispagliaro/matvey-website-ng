ef583350d8ddc399913f7426127cb672769b65c3
luispagliaro <luispagliaro@gmail>
2017-11-20 16:34:43 -0300

    Added news

--- src/app/core/data/shows.data.ts
+++ src/app/core/data/shows.data.ts
@@ -3,7 +3,7 @@ import { Shows } from '../../home/shared/shows.model';
 // { name: '', date: '', time: '', venue: '', address: '', lat: -34.619530, lng: -58.441316 },
 
 export let shows: Shows[] = [
-    { name: 'Matvey En La Colorada', date: 'Domingo 26 de Noviembre', time: '22:00', venue: 'La Colorada Music Bar', address: 'Honorio Pueyrredon 41, Caballito, Buenos Aires', lat: -34.619530, lng: -58.441316 },
+    { name: 'Matvey En La Colorada Music Bar', date: 'Domingo 26 de Noviembre', time: '19:30', venue: 'La Colorada Music Bar', address: 'Honorio Pueyrredon 41, Caballito, Buenos Aires', lat: -34.619530, lng: -58.441316 },
     { name: 'Matvey Junto A David Shankle', date: 'Sábado 2 de Diciembre', time: '21:00', venue: 'Parque Sarmiento', address: 'Av. Argentinas, Carcaraña, Santa Fe', lat: -32.840655, lng: -61.139523 },
     { name: 'Matvey En Rockan Bar', date: 'Viernes 15 de Diciembre', time: '22:00', venue: 'Rockan Bar', address: 'Buenos Aires 1092, Rosario, Santa Fe', lat: -32.950998, lng: -60.633691 }
 ];
